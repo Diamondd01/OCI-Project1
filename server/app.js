75047e1471969f5f7e4c01f848e9ca8eb550a952
Oracle Public Cloud User <opc@instance-project-oci2.sub04270045470.ociprojectvcn.oraclevcn.com>
2024-05-02 18:23:36 +0000

    added endpoints to project

--- server/app.js
+++ server/app.js
@@ -2,7 +2,8 @@ const bodyParser = require('express')
 const express = require('express');
 const pool = require('./db')
 
-const path = require('path')
+const path = require('path');
+const { error } = require('console');
 
 const app = express();
 
@@ -13,10 +14,43 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../dist')));
 
 // api router
-app.use('/test',(req,res,next)=>{
+app.get('/test',(req,res,next)=>{
   res.send('here')
   console.log('middlware for url')
   next()
+});
+
+app.get('/categories',async(req,res)=>{
+  try {
+    const result = await pool.query('SELECT name FROM Categories;');
+
+    const categoryNames = result.rows.map(row=>row.name)
+
+    res.json({categoryNames})
+  }catch(error){
+    console.error('Error retrieving information',error)
+    console.log(error)
+    
+  }
+});
+
+app.get('/random-dish/:categoryName',async(req,res)=>{
+  const {categoryNames} = req.params;
+  try{
+    const result = await pool.query(
+    'SELECT * FROM Recipes WHERE category_id = ( SELECT category_id FROM Categories WHERE name = $1',[categories])
+
+    if(result.rows.length === 0){
+      return res.status(400).json({error:'No recipe found'})
+    }
+    
+    const randomIndex = Math.floor(Math.random()* result.rows.length);
+    const randomRecipe = result.rows[randomIndex];
+    res.json(randomRecipe)
+  }catch(error){
+    console.error('Error retrieving random recipe',error);
+    res.status(500).json({error:'Internal server error'})
+  }
 });
 
 
